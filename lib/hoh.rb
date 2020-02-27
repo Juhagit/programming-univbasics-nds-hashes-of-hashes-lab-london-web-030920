494796f0ecd4dafada0c0df0d71437aea8025af9
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-27 02:07:50 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -35,7 +35,12 @@ def naming_system
  {label: "Kingdom",
  sub_category: {
    label: "Phylum",
-   
+   sub_category:{
+     label: "Class"
+     sub_category:{
+       label
+     }
+   }
  }
  
  
