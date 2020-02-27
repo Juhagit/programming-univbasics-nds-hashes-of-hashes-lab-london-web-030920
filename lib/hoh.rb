feb97a89afc2242fee520d899966720ca8263599
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-27 02:02:57 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,16 +4,16 @@
 #
 # The tests will guide your construction.
 #
- PORTION_1 = {
-   label: "Kingdom",
-   sub_category: {
-     label: "Phylum",
-     sub_category: {
-       label: "Class"
-     }
-   }
- }
- 
+# PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
+#   }
+# }
+# 
 # PORTION_2 = {
 #   label: "Order"
 # }
@@ -31,7 +31,9 @@
 # }
 
 def naming_system
- PORTION_1[:sub_category][:sub_category][:sub_category] 
+ 
+ 
+ 
  
   # Remember:
   #  Kingdom
